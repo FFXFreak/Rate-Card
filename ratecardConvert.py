4e87459bb566e5e4aa8f000e2c9f7223581657d7
Pearson <james.pearson@vodafone.com>
2022-02-23 12:07:05 +0000

    automated the process, now you can put 2 files into the Rate_Card folder and run the application

--- ratecardConvert.py
+++ ratecardConvert.py
@@ -3,24 +3,48 @@ import sys
 import xlrd
 
 import pandas as pd
+import glob, os
+
+input_folder = "\\Rate_Cards\\"
+output_folder = "\\Output_Rate_Cards\\"
+static_folder = "\\Static\\"
 
 def main():
 
-    #get filename
-    print("Please provide the name of the excel file without extention and ensure it is in the current folder")
-    filename = input("Filename: ")
+    files = []
 
-    #adds excel file and static files to dataframe and ensures no errors.
-    try:
-        df = pd.read_excel(filename + ".xlsx")
-    except:
-        sys.exit("Excel file either doesn't in folder on has been incorrectly inputted")
-    try:
-        static_df = pd.read_csv("Static\\static_rates.csv")
-    except:
-        sys.exit("Static.csv not in expected location")
+    #ensure files are in folder
+    print(
+        "Please ensure that all Rate Cards that need to be converted are in the Rate_Cards folder and you have included the static ratecard before you continue!")
+    run = input("Please enter Y to continue: ")
 
-    print("this will now extract from branch")
+    if run.upper() != "Y":
+        sys.exit("User not confirmed.")
+
+    #stores list of files in folder also changes current directory down a level
+    os.chdir(os.getcwd() + input_folder)
+    for file in glob.glob("*.xlsx"):
+        files.append(file)
+
+    #set the current directory back to the origin
+    os.chdir(os.path.dirname(os.getcwd()))
+
+    #ensures a min of 1 and max of 2 files in folder
+    if not files and len(files) < 3:
+        sys.exit("There are currently " + str(len(files)) + " files in the folder. Expecting Minimum of 1 and Maximum of 2")
+
+    #we now are going to run this script until all files have been processed
+    runtime = len(files)
+    while runtime != 0:
+        process_rate_card(runtime, files)
+        runtime -= 1
+    
+
+def process_rate_card(filenumber, files):
+    #adds excel file and static files to dataframe.
+    df = pd.read_excel(os.getcwd() + input_folder + files[filenumber - 1])
+    static_df = pd.read_csv(os.getcwd() + static_folder + "static_rates.csv")
+
     #rename and drop apropriate columns
     df = df.rename(columns={'SOC Code': 'SKU', 'Description': 'DESCRIPTION'})
     df['SKU'] = df.apply(lambda x: x['SKU']+"N" if x['Acq_Ret'] ==
@@ -55,7 +79,8 @@ def main():
     else:
         df['COMMISSION_WR'] = df['Leeds White Rose']
         df['COMMISSION_CAS'] = df['Castleford']
-        df['COMMISSION_GIG'] = df['Leeds White Rose']
+        df['Gigafast'] = df['Leeds White Rose']
+        df['COMMISSION_GIG'] = df['Gigafast']
 
         #print(df)
         for index, row in df.iterrows():
@@ -70,6 +95,7 @@ def main():
                 df.loc[index, "COMMISSION_WR"] = row['Leeds White Rose'] * .05
                 df.loc[index, "COMMISSION_CAS"] = row['Castleford'] * .05
                 df.loc[index, "COMMISSION_GIG"] = 0
+                df.loc[index, "Gigafast"] = 0
 
         df.drop(['Acq_Ret'], axis=1, inplace=True)
 
@@ -78,11 +104,11 @@ def main():
         gig_df = df.copy()
 
         wr_df = df.rename(columns={'Leeds White Rose': 'REVENUE', 'COMMISSION_WR': 'COMMISSION'})
-        wr_df.drop(['Leeds 8-9 Commercial Street','Castleford', 'COMMISSION_CAS', 'COMMISSION_GIG'], axis=1, inplace=True)
+        wr_df.drop(['Gigafast','Leeds 8-9 Commercial Street','Castleford', 'COMMISSION_CAS', 'COMMISSION_GIG'], axis=1, inplace=True)
         cas_df = df.rename(columns={'Castleford': 'REVENUE', 'COMMISSION_CAS': 'COMMISSION'})
-        cas_df.drop(['Leeds White Rose','Leeds 8-9 Commercial Street', 'COMMISSION_WR', 'COMMISSION_GIG'], axis=1, inplace=True)
-        gig_df = df.rename(columns={'Leeds White Rose': 'REVENUE', 'COMMISSION_GIG': 'COMMISSION'})
-        gig_df.drop(['Leeds 8-9 Commercial Street', 'COMMISSION_WR', 'COMMISSION_CAS'], axis=1, inplace=True)
+        cas_df.drop(['Gigafast','Leeds White Rose','Leeds 8-9 Commercial Street', 'COMMISSION_WR', 'COMMISSION_GIG'], axis=1, inplace=True)
+        gig_df = df.rename(columns={'Gigafast': 'REVENUE', 'COMMISSION_GIG': 'COMMISSION'})
+        gig_df.drop(['Castleford','Leeds White Rose','Leeds 8-9 Commercial Street', 'COMMISSION_WR', 'COMMISSION_CAS'], axis=1, inplace=True)
 
         wr_df = pd.concat([static_df, wr_df], ignore_index=True)
         cas_df = pd.concat([static_df, cas_df], ignore_index=True)
